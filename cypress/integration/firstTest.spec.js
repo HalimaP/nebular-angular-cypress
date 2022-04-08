6d3203dbc0bb55030f5781dede4ef5941d8c21f1
halima.potur <halima.potur@compusight.com>
2022-04-09 01:28:22 +0200

    Working with Lists and dropdowns; Working with tables in cypress

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -2,8 +2,7 @@
 
 /// <reference types="cypress"/>
 
-const { TreeNodeExpanderComponent } = require("angular-tree-component")
-const { ThemeManager } = require("tinymce")
+
 
 describe('Our first suite', () => {
 
@@ -159,5 +158,75 @@ it.only('check boxes', () =>{
     cy.get('[type ="checkbox"]').eq(1).check({force:true})
 
 })
+it.only('Lists and dropdowns', () => {
+    cy.visit('/')
+    cy.get('nav nb-select').click()
+    cy.get('.options-list').contains('Dark').click()
+    cy.get('nav nb-select').should('contain', 'Dark')
+    cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+    // Kako se ovdje radi o dropdown listi, sada da bismo provjerili da li je i sta oznaceno iz liste
+    // kod iznad mozemo ponoviti onoliko puta koliko imamo opcija za odabrati,
+    // ali da ne bismo duplicirali kod koristimo sljedeci nacin:
+    cy.get('nav nb-select').then(dropdown => {
+        cy.wrap(dropdown).click()
+        cy.get('.options-list nb-option').each((listItem, index) => {
+           const itemText = listItem.text().trim()
+           const colors={
+               "Light":"rgb(255, 255, 255)",
+               "Dark":"rgb(34, 43, 69)",
+               "Cosmic": "rgb(50, 50, 89)",
+               "Corporate":"rgb(255, 255, 255)"
+
+           }
+           cy.wrap(listItem).click()
+           cy.wrap(dropdown).should('contain', itemText)
+           cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+           if(index<3){
+            cy.wrap(dropdown).click()
+
+           }
+        })
+
+
+    })
+
+})
+it.only('Web tables', () => {
+    cy.visit('/')
+    cy.contains('Tables & Data').click()
+    cy.contains('Smart Table').click()
+    cy.get('tbody').contains('tr', 'Larry').then(tableRow=>{
+        cy.wrap(tableRow).find('.nb-edit').click()
+        cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25')
+        cy.wrap(tableRow).find('.nb-checkmark').click()
+        cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
+    })
+    cy.get('thead').find('.nb-plus').click()
+    cy.get('thead').find('tr').eq('2').then(tableRow =>{
+        cy.wrap(tableRow).find('[placeholder="First Name"]').type('Arthem')
+        cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Bondar')
+        cy.wrap(tableRow).find('.nb-checkmark').click()
+
+    })
+    cy.get('tbody tr').first().find('td').then(tableColumn =>{
+        cy.wrap(tableColumn).eq(2).should('contain', 'Arthem')
+        cy.wrap(tableColumn).eq(3).should('contain', 'Bondar')
+    })
+    const age =[20, 30, 40, 200]
+    cy.wrap(age).each(age =>{
+        cy.get('thead [placeholder="Age"]').clear().type(age)
+        cy.wait(500)
+        cy.get('tbody tr').each(tableRow =>{
+            if(age == 200){
+                cy.wrap(tableRow).should('contain', 'No data found')
+            }else{
+                cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+
+            }
+        })
+    })
+    
+})
+
 })
 
